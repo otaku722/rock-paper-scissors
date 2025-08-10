c704dc7d312754bb9d574693c01211581c158f08
Kalie <kaliecalnan@gmail.com>
2025-08-10 16:11:29 -0400

    chore: add pseudocode for getComputerChoice
    Add pseudocode explaining logic for the getComputerChoice function in script.js.

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+
+/* 
+
+Declare variable to store computer choice.
+
+Get a random number between 1 and 3.
+
+If number is 1:
+    return rock
+Else if number is 2:
+    return paper
+Else
+    return scissors
+
+*/
+
+}
