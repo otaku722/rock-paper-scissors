44d8de2c260cad6255270f4a6e88ee31cc0fd160
Kalie <kaliecalnan@gmail.com>
2025-08-10 20:16:33 -0400

    chore: add pseudocode for playRound
    Add pseudocode explaining logic for the playRound function in script.js.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-computer = getComputerChoice();
-human = getHumanChoice();
-
-console.log(human);
+// Track each player's score
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     // Stores random number between 1 and 3
@@ -21,4 +20,21 @@ function getHumanChoice() {
 
     // Return user input in lowercase
     return prompt("Rock, paper, or scissors?").toLowerCase();
+}
+
+function playRound(computerChoice, humanChoice) {
+    /* 
+    
+    Compare computer and human choices
+
+    If human beats computer:
+        Print "You win! Your choice beats the computer's choice"
+    Else if computer beats human:
+        Print "You lose! The computer's choice beats your choice"
+    Else:
+        Print "Tie!"
+
+    Increment winning player's score by 1
+
+    */
 }
